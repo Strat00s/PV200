a6bbb1ff320ea7672b578badf9dc2a3bbde6d342
stratos <l.bastyr@seznam.cz>
2023-12-07 18:37:21 +0100

    done

--- gate_finder/1hz_clock_div.v
+++ gate_finder/1hz_clock_div.v
@@ -1,20 +1,20 @@
 module clock_divider(
-    input clk,        // Input clock (50 MHz)
-    input reset,      // Synchronous reset
-    output reg out    // Output clock (1 Hz)
+    input clk,        // input clock (50 MHz)
+    input rst,
+    output reg out    // output clock
 );
 
 // Determine the number of bits for the counter. 
 // Since 25,000,000 is a 25-bit number, use a 25-bit counter.
 reg [24:0] counter;  
 
-always @(posedge clk or posedge reset) begin
-    if (reset) begin
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
         // Reset the counter and the output
         counter <= 0;
         out <= 0;
     end else begin
-        if (counter == 24_999_999) begin
+        if (counter == 1000000) begin
             // Reset the counter and toggle the output
             counter <= 0;
             out <= ~out;
