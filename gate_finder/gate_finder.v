3adf130c2bdebeb32ea7b8e7c7e5bfea4957669e
stratos <l.bastyr@seznam.cz>
2023-12-07 18:28:36 +0100

    done

--- gate_finder/gate_finder.v
+++ gate_finder/gate_finder.v
@@ -1,4 +1,4 @@
-// Sequential gate finder
+// Sequential (because easy) gate finder
 
 module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [35:0]GPIO_0);
 	assign rst = ~KEY[0];
@@ -21,7 +21,6 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
     assign GPIO_0[35] = pins_dir[9]  ? pins_out[9]  : 1'bz;
     assign GPIO_0[33] = pins_dir[10] ? pins_out[10] : 1'bz;
     assign GPIO_0[31] = pins_dir[11] ? pins_out[11] : 1'bz;
-
     // assign outputs
     assign pins_in[0]  = GPIO_0[24];
     assign pins_in[1]  = GPIO_0[22];
@@ -37,17 +36,7 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
     assign pins_in[11] = GPIO_0[31];
 
 
-    // slow clock for debuging
-    wire slow;
-	clock_divider s_clk(
-		.clk(CLOCK_50),
-		.reset(1'b0),
-		.out(slow)
-	);
-	
-	
-
-    // state
+    // States
     parameter RESET         = 0;
     parameter NOT_TEST      = 1;
     parameter AND_TEST      = 2;
@@ -55,24 +44,40 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
     parameter XOR_TEST      = 4;
     parameter FINISHED      = 5;
     parameter FAILED        = 6;
-    reg [7:0] state    = RESET; // current state
+    reg [7:0] state         = RESET; // Current state
 
 
-    // gpio read/write action
+    // GPIO read/write action
     parameter READ  = 0;
     parameter WRITE = 1;
-    // gpio in/out config
+    // GPIOgpio in/out config
     parameter IN  = 0;
     parameter OUT = 1;
-    reg rw = WRITE; //gpio read/write state
+    reg rw = WRITE; // GPIO read/write state
 
 
-    reg [9:0] result    = 0; // resulting LED pattern to show once gate type is determined
-    integer i           = 0;
-    integer test_val    = 0; // value for which to test the output for (0, 1, 10, ...)
-    integer func_match  = 0; // counter for determining if all function outputs were encountered
-    integer match_cnt   = 0; // valid gate output counter
-    reg reset_all     = 0;
+    reg [9:0] result     = 0; // Resulting LED pattern to show once gate type is determined (last 4 show what type is being tested/was found LEDs: NOT, AND, OR, XOR)
+    reg [7:0] i          = 0;
+    reg [7:0] test_val   = 0; // Value for which to test the output for (0, 1, 10, ...).
+    reg [7:0] func_match = 0; // Counter for determining if all function outputs were encountered.
+    reg [7:0] match_cnt  = 0; // Valid gate output counter
+    reg reset_all        = 0; // Go and reset everything
+
+
+    // Registeres for finding valid gate when not enough matches were encountered (most gates are dead)
+    reg [7:0] match_cnts[3:0];
+    reg [7:0] max   = 0;
+    reg [7:0] index = 0;
+
+
+    // Slow clock for making it look somewhat nice
+    wire slow;
+	clock_divider s_clk(
+		.clk(CLOCK_50),
+		.rst(1'b0),
+		.out(slow)
+	);
+
 
     always @(posedge slow, posedge rst) begin
         // go to rst state on rset
@@ -83,11 +88,17 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
         else if (state == RESET) begin
             reset_all = 1;
             state     = NOT_TEST;
+				match_cnts[0] = 0;
+				match_cnts[1] = 0;
+				match_cnts[2] = 0;
+				match_cnts[3] = 0;
         end
 
         // reset everything
         else if (reset_all) begin
             reset_all  = 0;
+				max        = 0;
+				index      = 0;
             rw         = WRITE;
             test_val   = 0;
             func_match = 0;
@@ -106,9 +117,9 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
 
         // Start with NOT
         else if (state == NOT_TEST) begin
-            result[9] = 1; // last LED is for NOT
-            // Go through every possible gate for NOT (3 on each side)
-            // Write to pins
+            result[9] = 1; // Show what gate type we are testing
+
+            // Write to pins first
             if (rw == WRITE) begin
                 rw = READ;
                 pins_out[i] = test_val[0];
@@ -116,11 +127,11 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
                 test_val = test_val + 1;
             end
 
-            // Read from pins on next clock
+            // Then read the pins
             else begin
                 rw = WRITE;
 
-                // NOT -> match if input is not output
+                // NOT -> A not X
                 if (pins_out[i] == ~pins_in[i + 1])
                     func_match = func_match + 1;
 
@@ -128,7 +139,7 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
                 if (test_val == 2) begin
                     test_val = 0;
 
-                    // if we have a match, it is most likely a valid NOT gate
+                    // if we have a full function match, it is most likely a valid NOT gate
                     if (func_match == 2) begin
                         match_cnt = match_cnt + 1;
                         result[i / 2] = 1;
@@ -139,11 +150,15 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
                 end
             end
 
+            // All pins traversed
             if (i >= 12) begin
+                // At least half has to work to be valid right away
                 if (match_cnt >= 3) begin
                     state = FINISHED;
                 end
                 else begin
+                    // Save match if not other gate is determied
+                    match_cnts[3] = result[5:0];
                     reset_all = 1;
                     state = AND_TEST;
                 end
@@ -191,6 +206,7 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
                     state = FINISHED;
                 end
                 else begin
+                    match_cnts[2] = result[5:0];
                     reset_all = 1;
                     state = OR_TEST;
                 end
@@ -239,6 +255,7 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
                     state = FINISHED;
                 end
                 else begin
+                    match_cnts[1] = result[5:0];
                     reset_all = 1;
                     state = XOR_TEST;
                 end
@@ -287,6 +304,7 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
                     state = FINISHED;
                 end
                 else begin
+                    match_cnts[0] = result[5:0];
                     reset_all = 1;
                     state = FAILED;
                 end
@@ -298,16 +316,28 @@ module gate_finder(input CLOCK_50, input [3:0]KEY, output reg [9:0]LEDR, inout [
             // nothing to see here. Just wait for a reset
         end
 
+        // Find the best match
         else if (state == FAILED) begin
-            // nothing to see here. Just wait for a reset
+            state = FINISHED;
+
+				// Find best match (if there is any)
+				for (i = 0; i < 4; i = i + 1) begin
+                if (max < match_cnts[i]) begin
+                    max = match_cnts[i];
+                    index = i;
+                end
+            end
+
+            // No gate was found
+            if (max == 0)
+                reset_all = 1;
+            else begin
+                result[index + 6] = 1;
+                result[5:0] = max;
+            end
         end
 
+        // Show progress and final result
         LEDR = result;
-
 	end
-	
-	
-	
-	
-	
 endmodule
